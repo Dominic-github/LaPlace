dfff4f9f9e3423bff7cc02bf2a1f14c05bef5a31
Dominic-github <78993276+Dominic-github@users.noreply.github.com>
2025-10-20 20:55:30 +0700

    feat: remove node_module, .vscode

---
+++ server/src/config/database.js
@@ -0,0 +1,32 @@
+import { Sequelize } from 'sequelize'
+import mysql from 'mysql2/promise'
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  { host: process.env.DATABASE_HOST, dialect: 'mysql', logging: false }
+)
+
+export default {
+  sequelize,
+  connect: async () => {
+    try {
+      const connection = await mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD
+      })
+
+      await connection.query('CREATE DATABASE IF NOT EXISTS `clothes-web-shop`')
+
+      await connection.end()
+
+      await sequelize.authenticate()
+      console.log('Connection has been established successfully.')
+      await sequelize.sync()
+    } catch (error) {
+      console.error('Unable to connect to the database:', error)
+    }
+  }
+}
